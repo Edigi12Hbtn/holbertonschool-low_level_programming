e2c4ec0c7fcb117a87a7b4273bd2ddc8b6f6746a
Edison Giraldo A <1470@holbertonschool.com>
2020-02-06 21:38:15 +0000

    Exc 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n <= 9)
+	{
+		putchar(n + 48);
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n += 1;
+	}
+	putchar('\n');
+	return (0);
+}
