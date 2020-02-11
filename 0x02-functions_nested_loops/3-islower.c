6401d7f99a9e7f0da3869a80edd6d68117d57edd
Edison Giraldo A <1470@holbertonschool.com>
2020-02-11 19:05:23 +0000

    Exc 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+/**
+ * int _islower - Entry point
+ *
+ * @c: - is a parameter
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c);
+{
+	char letter = 'a';
+	int value = 0;
+
+	while (letter <= 'z')
+	{
+		if (c == letter)
+			value = 1;
+		letter++;
+	}
+	return (value);
+}
