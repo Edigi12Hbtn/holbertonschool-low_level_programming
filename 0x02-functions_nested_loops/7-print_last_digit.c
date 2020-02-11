2ba0cae34dd6ed00fa0afc4a08fda354317c16a6
Edison Giraldo A <1470@holbertonschool.com>
2020-02-11 20:02:49 +0000

    Exc 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - Entry point
+ *
+ * @n: - number
+ *
+ * Return: Always 0 (Success)
+ */
+
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	last_digit = _abs(n) % 10;
+	_putchar(last_digit + '0');
+
+	return (last_digit);
+}
