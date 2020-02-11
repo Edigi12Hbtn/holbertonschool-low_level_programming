29da4a786ec63f13d1f96b268ed662210feed7bd
Edison Giraldo A <1470@holbertonschool.com>
2020-02-11 20:12:35 +0000

    Exc 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,7 +12,11 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	last_digit = _abs(n) % 10;
+	last_digit = n;
+	if (n < 0){
+		last_digit *= -1;
+	}
+	last_digit %= 10;
 	_putchar(last_digit + '0');
 
 	return (last_digit);
