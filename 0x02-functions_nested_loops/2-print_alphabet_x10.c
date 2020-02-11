b529837399f0b589696fdc83dba685857ed6a489
Edison Giraldo A <1470@holbertonschool.com>
2020-02-11 18:40:37 +0000

    Exc 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int times = 0;
+
+	while (times < 10)
+	{
+		letter = 'a';
+
+		while (letter <= 'z')
+		{
+			_putchar(letter++);
+		}
+		_putchar('\n');
+		times++
+	}
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -2,4 +2,5 @@
 #define _SAMPLE_HEADER_H_
 int _putchar(char c);
 void print_alphabet(void);
+void print_alphabet_x10(void);
 #endif /* _SAMPLE_HEADER_H_ */
