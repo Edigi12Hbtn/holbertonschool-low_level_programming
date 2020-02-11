7df0512eac6dd3961c416cf3ab6da781e291d12d
Edison Giraldo A <1470@holbertonschool.com>
2020-02-11 18:17:16 +0000

    Exc 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+/**
+ * print_alphabet() - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
