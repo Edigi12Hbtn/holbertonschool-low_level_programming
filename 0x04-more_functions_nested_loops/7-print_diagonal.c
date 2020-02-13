b5c67fbb2c76573bf378e6386a62b0b5e8e7b1e6
Edison Giraldo A <1470@holbertonschool.com>
2020-02-13 20:45:47 +0000

    Exc 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal- Entry point
+ *
+ * @n: - number;
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int l, c;
+
+	for (l = 0; l < n; l++)
+	{
+		for (c = 0; c < (l - 1), c++)
+			_putchar(' ');
+		_putchar('\');
+		if (l != n)
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
