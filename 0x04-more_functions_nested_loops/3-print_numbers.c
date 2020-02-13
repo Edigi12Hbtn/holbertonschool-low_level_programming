c8ce3ad51c3b6728a3a328e1a1b428d8570d118a
Edison Giraldo A <1470@holbertonschool.com>
2020-02-13 19:29:17 +0000

    Exc 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,14 @@
 #include "holberton.h"
-
 /**
  * print_numbers - Entry point
  *
- * Return: Always 0 (Success)
  */
 
 void print_numbers(void)
 {
-	for (int i = 0; i < 10, i++)
-	{
+	int i;
+
+	for (i = 0; i < 10; i++)
 		_putchar(i + '0');
-	}
 	_putchar('\n');
 }
