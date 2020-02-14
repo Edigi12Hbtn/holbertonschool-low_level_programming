90555d40259f53b6a1fa46facdd71179634210c3
Edison Giraldo A <1470@holbertonschool.com>
2020-02-14 02:45:37 +0000

    Exc 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,7 +14,6 @@ void print_number(int n)
 	if (n == 0)
 	{
 		Pd = 10;
-	/*	printf("cero\n");*/
 	}
 	else if (n > 0)
 	{
@@ -23,13 +22,11 @@ void print_number(int n)
 			if ((n >= m) && n < (m * 10))
 				Pd = m * 10;
 		}
-	/*	printf("positivo\n");*/
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
-	/*	printf("valor de n neg: %d\n", n);*/
 		for (m = 1; m <= 100000; m *= 10)
 		{
 			if ((n >= m) && (n < (m * 10)))
@@ -39,12 +36,8 @@ void print_number(int n)
 
 	for (i = Pd; i >= 10; i /= 10)
 	{
-	/*	printf("loop final, n es: %d\n", n);*/
-	/*	printf("%d hi\n", i);*/
 		d = n % i;
-	/*	printf("%d nmodi\n", d);*/
 		d = d / (i / 10);
-	/*	printf("%d ndivi\n", d);*/
 		_putchar(d + '0');
 	}
 }
