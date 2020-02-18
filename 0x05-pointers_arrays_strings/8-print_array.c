327a150642050dc10add8b54f145bbbbe2b81dbb
Edison Giraldo A <1470@holbertonschool.com>
2020-02-18 20:13:20 +0000

    Exc 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - print the first n integers of a array
+ *
+ * @a: array
+ * @n: number of integers to print
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for(i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i != (n - 1))
+			printf(", ");
+		else
+			printf("\n");
+	}
+}
