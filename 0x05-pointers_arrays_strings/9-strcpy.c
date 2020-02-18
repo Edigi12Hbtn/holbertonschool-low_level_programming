0e2200cbbc7638bbc7628fa88caf2c796046b037
Edison Giraldo A <1470@holbertonschool.com>
2020-02-18 20:51:17 +0000

    Exc 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - copies the string pointed by src to the buffer pointed for dest
+ *
+ * @dest: Buffer where will be stored the value in *src
+ * @src: Source of data for buffer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int index;
+
+	index = 0;
+	while (src[index] != '\0')
+	{
+		*(dest + index) = src[index];
+		index++;
+	}
+	*(dest + index) = '\0';
+	return (dest);
+}
