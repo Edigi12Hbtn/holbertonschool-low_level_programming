76d7ac0f7d8e1e7965aa96096d4728cc6ac7462a
Edison Giraldo A <1470@holbertonschool.com>
2020-02-20 20:36:19 +0000

    Exc 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,6 +3,8 @@
 /**
  * string_toupper - change lowercase to uppercase
  *
+ * @arr: array to change
+ *
  * Return: return string with uppercase
  */
 
@@ -10,12 +12,12 @@ char *string_toupper(char *arr)
 {
 	int i;
 
-	while(arr[i] != '\0')
+	while (arr[i] != '\0')
 	{
-		if(arr[i] >= 'a' && arr[i] <= 'z')
+		if (arr[i] >= 'a' && arr[i] <= 'z')
 			arr[i] -= 32;
 		i++;
 	}
 
-	return(arr);
+	return (arr);
 }
