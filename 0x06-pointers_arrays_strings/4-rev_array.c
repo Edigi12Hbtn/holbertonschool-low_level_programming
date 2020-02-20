47ae4246849fbde8bb3babf865d3461e9ff83599
Edison Giraldo A <1470@holbertonschool.com>
2020-02-20 20:25:43 +0000

    Exc 0

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reverse the contents of an integer array
+ *
+ * @a: array of integers
+ * @n: number of elements in array a
+ *
+ * Return: Always 0 (Success)
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, arr[10000];
+
+	i = 0;
+	while(i < n)
+	{
+		arr[i] = a[i];
+		i++;
+	}
+	i = 0;
+	while(i < n)
+	{
+		a[i] = arr[n - 1 - i];
+		i++;
+	}
+}
