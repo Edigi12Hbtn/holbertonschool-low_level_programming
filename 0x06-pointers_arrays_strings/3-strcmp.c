8adb960cf707194e09386d890d4329d5a034fe57
Edison Giraldo A <1470@holbertonschool.com>
2020-02-20 19:42:53 +0000

    Exc 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - compare strings
+ *
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int len_s1, len_s2;
+
+	len_s1 = 0;
+	len_s2 = 0;
+
+	while (s1[len_s1] != '\0')
+		len_s1++;
+
+	while (s2[len_s2] != '\0')
+		len_s2++;
+
+	return (len_s1 - len_s2);
+}
