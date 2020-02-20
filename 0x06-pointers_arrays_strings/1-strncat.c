a29f890a706075ce6ad89e2dfeb0275c96a413dd
Edison Giraldo A <1470@holbertonschool.com>
2020-02-20 18:16:50 +0000

    Exc 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _strncat - concat two strings
+ *
+ * @dest: destine
+ * @src: source
+ *
+ * Return: returns the pointer that points to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int pos_max_dest, i;
+
+	pos_max_dest = 0;
+
+	while (dest[pos_max_dest] != '\0')
+		pos_max_dest++;
+	pos_max_dest--;
+
+	for (i = pos_max_dest + 1; i <= (pos_max_dest + n); i++)
+		dest[i] = src[i - (pos_max_dest + 1)];
+	dest[i] = '\0';
+
+	return (dest);
+}
