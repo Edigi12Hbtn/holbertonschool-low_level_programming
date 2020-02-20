523f3364a9b4da5735879d3753ab8efd3fe4fa85
Edison Giraldo A <1470@holbertonschool.com>
2020-02-20 19:19:33 +0000

    Exc 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -19,7 +19,11 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		dest[i] = src[i];
 		if (src[i] == '\0')
+		{
+			while(dest[i] != '\0')
+				dest[i++] = '\0';
 			break;
+		}
 		i++;
 	}
 	return (dest);
