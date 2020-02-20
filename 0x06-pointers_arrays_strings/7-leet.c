61dbe6b7290f1995b22402a4e1c258fac76afee3
Edison Giraldo A <1470@holbertonschool.com>
2020-02-20 23:29:57 +0000

    Exc 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * leet - Entry point
+ * @s: string
+ * Return: Always 0 (Success)
+ */
+
+char *leet(char *s)
+{
+	char arr_1[] = "aeotl";
+	char arr_2[] = "43071";
+	int j = 0, i = 0;
+
+	while (s[i] != '\0')
+	{
+		for (j = 0 ; j < 5; j++)
+		{
+			if ((s[i] == arr_1[j]) || (s[i] == arr_1[j - 32]))
+				s[i] = arr_2[j];
+		}
+		i++;
+	}
+	return (s);
+}
