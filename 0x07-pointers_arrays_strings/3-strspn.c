7a6f37d00bcf84e198f467c96f0b1da24522d3df
Edison Giraldo A <1470@holbertonschool.com>
2020-02-24 19:27:24 +0000

    Exc 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
 
 /**
- * int _strspn -  gets the length of a prefix substring.
+ * _strspn -  gets the length of a prefix substring.
  *
  * @s: string
  * @accept: letters for match
  *
- * Return: Returns the number of bytes in the initial segment of s which consist only of bytes from accept.
+ * Return: Returns the number of bytes in the initial
+ * segment of s which consist only of bytes from accept.
  */
 
 unsigned int _strspn(char *s, char *accept)
