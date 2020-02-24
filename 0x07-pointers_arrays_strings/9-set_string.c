0a0904ee3de2151c6ad83fffd965033a333e87d0
Edison Giraldo A <1470@holbertonschool.com>
2020-02-24 23:47:56 +0000

    Exc 9

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -1,9 +1,13 @@
 #include "holberton.h"
 
 /**
- * main - Entry point
+ * set_string - sets the value of a pointer to a char.
  *
- * Return: Always 0 (Success)
+ * @s: pointer to pointer
+ * @to: pointer to
  */
 
-
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
