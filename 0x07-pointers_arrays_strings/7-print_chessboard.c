11ef474729596c58bba5e5df6e8f7dd34c729d80
Edison Giraldo A <1470@holbertonschool.com>
2020-02-24 21:20:22 +0000

    Exc 7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * print_chessboard - function that prints the chessboard..
+ * print_chessboard - function that prints the chessboard.
  *
  * @a: chessboard matrix
  */
@@ -9,6 +9,7 @@
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
+
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
