1b2c2bfecc58683cdd1eb3176986545a63fa10ae
Edison Giraldo A <1470@holbertonschool.com>
2020-02-24 20:08:55 +0000

    Exc 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,9 +1,30 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
- * main - Entry point
+ * _strpbrk - locates the first occurrence in the string s
+ * of any of the bytes in the string accept.
  *
- * Return: Always 0 (Success)
+ * @s: string
+ * @accept: letters for match
+ *
+ * Return: Returns a pointer to the byte in s that matches
+ * one of the bytes in accept, or NULL if no such byte is found.
  */
 
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+		}
+	}
+	return (NULL);
+}
