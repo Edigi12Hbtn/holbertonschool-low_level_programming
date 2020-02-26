34d9e7a7c2f9fbb147df29e3d83547a233222e12
Edison Giraldo A <1470@holbertonschool.com>
2020-02-26 21:07:25 +0000

    Exc 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - prints s in reverse.
+ *
+ * @s: string.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(++s);
+		s--;
+		_putchar(*s);
+	}
+	else
+		s--;
+}
