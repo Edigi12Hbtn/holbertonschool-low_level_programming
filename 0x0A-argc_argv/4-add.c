d9dd8a6d1c2fd36f6025eed1269b627ce9c339e1
Edison Giraldo A <1470@holbertonschool.com>
2020-02-29 19:18:58 +0000

    Exc 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,9 +1,40 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - Entry point
+ * main - main function
  *
- * Return: Always 0 (Success)
+ * @argc: number of command line arguments.
+ * @argv: pointers to parameters inserted on the command line.
+ *
+ * Return: return sucess.
  */
 
+int main(int argc, char *argv[])
+{
+	int num, i, j;
 
+	num = 0;
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; *(argv[i] + j) != '\0'; j++)
+			{
+				if (*(argv[i] + j) < '0' || *(argv[i] + j) > '9')
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			num += atoi(argv[i]);
+		}
+		printf("%d\n", num);
+	}
+	return (0);
+}
