74a72af7f4f32bc0f2ee28915d33ed8d79178ba4
Edison Giraldo A <1470@holbertonschool.com>
2020-02-29 18:21:53 +0000

    Exc 0

---
+++ 0x0A-argc_arg/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * @argc: number of command line arguments.
+ * @argv: pointers to parameters inserted on the command line.
+ *
+ * Return - return sucess.
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (1);
+}
