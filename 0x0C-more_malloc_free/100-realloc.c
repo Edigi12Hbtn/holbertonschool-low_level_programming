3163e2038f512363197aff9f3335ab86fb5bed3c
Edison Giraldo A <1470@holbertonschool.com>
2020-03-05 23:47:49 +0000

    Advanced Exercise

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -40,7 +40,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			min = old_size;
 		else
 			min = new_size;
-	
+
 		for (i = 0; i < min; i++)
 			pointer_2[i] = pointer[i];
 		free(ptr);
