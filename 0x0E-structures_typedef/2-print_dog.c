78e86e5c2b782c79f1fef24817389bfca4958bb1
Edison Giraldo A <1470@holbertonschool.com>
2020-03-10 02:10:36 +0000

    Exc 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - print dog structure.
+ *
+ * @d: pointer to dog structure.
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: %s\n", "(nil)");
+		else
+			printf("Name: %s\n", d->name);
+		if (d->age == 0)
+			printf("Age: %s\n", "(nil)");
+		else
+			printf("Age: %.6f\n", d->age);
+		if (d->owner == NULL)
+			printf("Owner: %s\n", "(nil)");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
+}
