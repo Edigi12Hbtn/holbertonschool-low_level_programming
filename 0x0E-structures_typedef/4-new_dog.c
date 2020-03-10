c6bbd9ea5d844f1d42a3371fed6da5a69a0354cd
Edison Giraldo A <1470@holbertonschool.com>
2020-03-10 03:01:15 +0000

    Exc 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,30 +20,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
-
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
-
 	while (name[size_of_name_p1] != '\0')
 		size_of_name_p1++;
 	size_of_name_p1++;
-
 	new_name = malloc(size_of_name_p1);
 	if (new_name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-
 	for (i = 0; i < size_of_name_p1 - 1; i++)
 		new_name[i] = name[i];
 	new_name[i] = '\0';
-
 	while (owner[size_of_owner_p1] != '\0')
 		size_of_owner_p1++;
 	size_of_owner_p1++;
-
 	new_owner = malloc(size_of_owner_p1);
 	if (new_owner == NULL)
 	{
@@ -54,10 +48,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (i = 0; i < size_of_owner_p1 - 1; i++)
 		new_owner[i] = owner[i];
 	new_owner[i] = '\0';
-
 	new_dog->name = new_name;
 	new_dog->age = age;
 	new_dog->owner = new_owner;
-
 	return (new_dog);
 }
