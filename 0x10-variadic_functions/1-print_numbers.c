3086e089807c7517b3bf6126cbb9978a715b37c9
Edigi12Hbtn <1470@holbertonschool.com>
2020-03-14 00:02:59 +0000

    c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,8 +1,27 @@
-#include "holberton.h"
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
- * main - Entry point
+ * print_numbers - print numbers.
  *
- * Return: Always 0 (Success)
+ * @separator: separator of printed numbers.
+ * @n: number of elements to print.
  */
 
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	if (separator != NULL && n != 0)
+	{
+		va_list ap;
+		unsigned int i;
+
+		va_start(ap, n);
+		for (i = 0; i < n - 1; i++)
+		{
+			printf("%d%s", va_arg(ap, int), separator);
+		}
+		printf("%d\n", va_arg(ap, int));
+		va_end(ap);
+	}
+}
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,8 +1,29 @@
-#include "holberton.h"
+#include "variadic_funcions.h"
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
- * main - Entry point
+ * print strings - prints strings, followed by a new line.
  *
- * Return: Always 0 (Success)
+ * @separator: string to be printed between the strings.
+ * @n: number of strings passed to the function.
  */
 
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	if (separator != NULL && n != 0)
+	{
+		va_list ap;
+		char *p;
+		unsigned int i;
+
+		va_start(ap, separator);
+		for (i = 0; i < n - 1; i++)
+		{
+			p = va_arg(ap, char*);
+			if (p != NULL)
+			printf("%s%s", va_arg(ap, char*), separator)
+		}
+		va_end(ap);
+	}
+}
