d22b3c8883c219e06c972d8efb83f81976fa2d0a
Edigi12Hbtn <1470@holbertonschool.com>
2020-03-14 22:03:59 +0000

    Ëxc3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,8 +1,58 @@
-#include "holberton.h"
+#include "variadic_functions.h"
+#include <stdio.h>
+
+void print_char(va_list ap)
+{
+	printf("%c, ", va_arg(ap, int));
+}
+
+void print_integer(va_list ap)
+{
+	printf("%i, ", va_arg(ap, int));
+}
+
+void print_float(va_list ap)
+{
+	printf("%f, ", va_arg(ap, double));
+}
+
+void print_string(va_list ap)
+{
+	char *str = va_arg(ap, char*);
+
+	if (str != NULL)
+		printf("%s, ", str);
+	else
+		printf("(nil), ");
+}
 
 /**
- * main - Entry point
+ * print_all - print 4 kind of variables.
  *
- * Return: Always 0 (Success)
+ * @format: format of incomming variables.
  */
 
+void print_all(const char * const format, ...)
+{
+	int i = 0, j = 0;
+	struct print_func char_to_func[] ={{'c', print_char},
+		{'i', print_integer},{'f', print_float},{'s', print_string}};
+	va_list ap;
+
+	va_start(ap, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+		{
+			j = 0;
+			while (format[i] != char_to_func[j].c)
+			{
+				j++;
+			}
+			char_to_func[j].f(ap);
+		}
+		i++;
+	}
+	va_end(ap);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,12 @@
 #ifndef VARIADIC_H
 #define VARIADIC_H
+#include <stdarg.h>
+
+struct print_func
+{
+	char c;
+	void (*f)(va_list ap);
+};
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
