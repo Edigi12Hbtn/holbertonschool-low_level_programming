d2a5670ff9ca687a693ff5603012c912e26ba46b
Edison Giraldo A <1470@holbertonschool.com>
2020-03-16 00:22:39 +0000

    Exc 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,20 +1,45 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 
+/**
+ * print_char - prints a char.
+ *
+ * @ap: list with parameter to print.
+ */
+
+
 void print_char(va_list ap)
 {
 	printf("%c", va_arg(ap, int));
 }
+
+/**
+ * print_integer - prints an integer.
+ *
+ * @ap: list with parameter to print.
+ */
 
 void print_integer(va_list ap)
 {
 	printf("%i", va_arg(ap, int));
 }
 
+/**
+ * print_float - prints a float.
+ *
+ * @ap: list with parameter to print.
+ */
+
 void print_float(va_list ap)
 {
 	printf("%f", va_arg(ap, double));
 }
+
+/**
+ * print_string - prints a string.
+ *
+ * @ap: list with parameter to print.
+ */
 
 void print_string(va_list ap)
 {
@@ -35,20 +60,20 @@ void print_all(const char * const format, ...)
 {
 	int i = 0, j = 0;
 	char *separator = "";
-	struct print_func char_to_func[] ={{'c', print_char},
-		{'i', print_integer},{'f', print_float},{'s', print_string}};
+
+	print_func char_to_func[] = {{'c', print_char},
+		{'i', print_integer}, {'f', print_float}, {'s', print_string}};
 	va_list ap;
 
 	va_start(ap, format);
 	while (format[i] != '\0')
 	{
-		if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+		if (format[i] == 'c' || format[i] == 'i' ||
+		format[i] == 'f' || format[i] == 's')
 		{
 			j = 0;
 			while (format[i] != char_to_func[j].c)
-			{
 				j++;
-			}
 			printf("%s", separator);
 			char_to_func[j].f(ap);
 			separator = ", ";
