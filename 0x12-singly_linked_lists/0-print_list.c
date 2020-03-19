b937cc7ad19547ff863db813dfb209bed5c4a152
Edison Giraldo A <1470@holbertonschool.com>
2020-03-19 22:30:49 +0000

    Exc 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * print_list - prints linked lists elements.
+ * 
+ * @h: pointer to header list.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+	char *string;
+	const list_t *current_list;
+
+	current_list = h;
+	while (current_list->next != NULL)
+	{
+		n++;
+		string = current_list->str;
+		if (string != NULL)
+			printf("[%u] %s\n", current_list->len, string);
+		else
+			printf("[0] (nil)\n");
+		current_list = current_list->next;
+	}
+
+	n++;
+	string = current_list->str;
+	if (string != NULL)
+		printf("[%u] %s\n", current_list->len, string);
+	else
+		printf("[0] (nil)\n");
+
+	return (n);
+}
