b8d334ce1c36034d7ddd41b0efa17f5d9a4b3645
Edison Giraldo A <1470@holbertonschool.com>
2020-03-19 22:45:00 +0000

    Exc 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,23 +15,27 @@ size_t print_list(const list_t *h)
 	const list_t *current_list;
 
 	current_list = h;
-	while (current_list->next != NULL)
+
+	if (current_list != NULL)
 	{
+		while (current_list->next != NULL)
+		{
+			n++;
+			string = current_list->str;
+			if (string != NULL)
+				printf("[%u] %s\n", current_list->len, string);
+			else
+				printf("[0] (nil)\n");
+			current_list = current_list->next;
+		}
+
 		n++;
 		string = current_list->str;
 		if (string != NULL)
 			printf("[%u] %s\n", current_list->len, string);
 		else
 			printf("[0] (nil)\n");
-		current_list = current_list->next;
 	}
-
-	n++;
-	string = current_list->str;
-	if (string != NULL)
-		printf("[%u] %s\n", current_list->len, string);
-	else
-		printf("[0] (nil)\n");
 
 	return (n);
 }
