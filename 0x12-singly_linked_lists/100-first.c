ddca24fe670c56c486d9fdc0a8af506e75e74723
Edison Giraldo A <1470@holbertonschool.com>
2020-03-20 02:59:56 +0000

    Exc advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * print - prints a text before the main.
+ */
+
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\nI");
+	printf(" bore my house upon my back!\n");
+}
