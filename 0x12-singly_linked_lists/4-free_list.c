65d952f065baf2e09a7bc9eec5b7b714e0358705
Edison Giraldo A <1470@holbertonschool.com>
2020-03-20 02:43:13 +0000

    Exc 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,10 +1,14 @@
 #include "lists.h"
-
+/**
+ * free_list - free memory.
+ *
+ * @head: first list.
+ */
 void free_list(list_t *head)
 {
 	while (head != NULL)
 	{
-		/*free(head->str);*/
+		free(head->str);
 		free(head);
 		head = head->next;
 	}
