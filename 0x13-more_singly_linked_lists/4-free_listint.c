cfac51602d3873f2deffa7e8cd2b5d22f3894fcc
Edison Giraldo A <1470@holbertonschool.com>
2020-03-23 21:17:29 +0000

    Exc4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * free_listint -  frees a listint_t list.
+ *
+ * @head: head of linked list.
+ */
+
+void free_listint(listint_t *head)
+{
+	while (head != NULL)
+	{
+		free(head);
+		head = head->next;
+	}
+}
