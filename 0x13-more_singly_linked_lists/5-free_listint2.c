39505e9943481d02b44ed2f30e2209eb47040cf5
Edison Giraldo A <1470@holbertonschool.com>
2020-03-23 21:17:45 +0000

    Exc5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * free_listint2 -  frees a listint_t list.
+ *
+ * @head:head pointer to start of linked list.
+ */
+
+void free_listint2(listint_t **head)
+{
+	while (*head != NULL)
+	{
+		free(*head);
+		*head = (*head)->next;
+	}
+}
