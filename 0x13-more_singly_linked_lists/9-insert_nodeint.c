102dc9baef893c2f3fa2bfb54b190aa1d56032dc
Edison Giraldo A <1470@holbertonschool.com>
2020-03-24 15:44:17 +0000

    Exc9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -41,8 +41,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 	}
 	else
+	{
 		new_node->next = current_node->next;
-	current_node->next = new_node;
+		current_node->next = new_node;
+	}
 
 	return (current_node);
 }
