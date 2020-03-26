c58a6f68c4694c3c21002ef35ce750439e3700bd
Edison Giraldo A <1470@holbertonschool.com>
2020-03-26 19:41:22 +0000

    Exc0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ *
+ * @b: binari number.
+ *
+ * Return: the converted number, or 0 if
+ * there is one or more chars in the string b that is not 0 or 1
+ * b is NULL.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0, idx = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while(b[idx] != '\0')
+	{
+		num <<= 1;
+
+		if (b[idx] != '0' && b[idx] != '1')
+			return (0);
+		else
+			num += b[idx] - 48;
+
+		idx++;
+	}
+
+	return (num);
+}
