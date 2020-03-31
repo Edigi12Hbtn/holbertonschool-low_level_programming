5e96d40f4a6079b3b4eb59c36ce43876ce5f73b8
Edison Giraldo A <1470@holbertonschool.com>
2020-03-31 22:56:13 +0000

    Exc 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t ret = 0, written_bytes = 0;
+	int fd; /* file descriptor */
+	char *str; /*Buffer to store readed data*/
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY); /* open a file like read and write */
+
+	if (fd == -1)
+		return (0);
+
+	str = malloc(letters);
+	if (str == NULL)
+		return (0);
+
+	ret = read(fd, str, letters); /* numbers of readed bytes*/
+
+	if (ret == -1)
+		return (0);
+
+	close(fd);
+
+	written_bytes = write(STDOUT_FILENO, str, ret);
+
+	if (written_bytes == -1 || written_bytes != ret)
+		return (0);
+
+	return (ret);
+}
