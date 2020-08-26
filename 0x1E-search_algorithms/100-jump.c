8e68bff43fb9a9d823d9b14cb86fb4f668b551ce
Edison Girado A <1470@Holbertonschool.com>
2020-08-26 15:46:03 -0500

    searches for a value in a sorted array of integers using the Jump search algorithm.

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,7 +13,7 @@ int jump_search(int *array, size_t size, int value)
 {
 	size_t start = 0, end = sqrt(size), i = 0;
 
-	while(array[end] < value && end < size)
+	while(end < size && array[end] < value)
 	{
 		printf("Value checked array[%d] = [%d]\n", (int) start, array[start]);
 		start = end;
